5ec177fff0706cc122c1dcfa009b1cae3b0a01ff
abdelrahman <abdelrahman18@gmail.com>
2023-09-04 01:17:46 +0300

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,21 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Buggy implementation of nextNumber! but i fixed it now :))) */
+    /** this method is used to print the collatz sequence starting from a given number
+     * it has three condition : if the number is even , odd or 1;
+     * in even number case it should return n/2
+     * in odd number case it should return 3n+1
+     * or if it was 1 the sequence ends and returns 1
+     * 
+     * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n  == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        } else if (n % 2 == 0) {
+            return n / 2 ;
         } else {
-            return n * 2;
+            return 3*n+1;
         }
     }
 
